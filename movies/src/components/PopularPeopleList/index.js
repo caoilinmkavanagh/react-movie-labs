2ac2f3e470cd3029e31cda3e30f004386ad4ca11
Caoilin Kavanagh <caoilinmkavanagh@gmail.com>
2023-08-02 14:42:20 +0100

    added according display of details for individual person.

--- movies/src/components/PopularPeopleList/index.js
+++ movies/src/components/PopularPeopleList/index.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getPopularPeople } from '../../api/tmdb-api';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const PopularPeopleList = () => {
   const [people, setPeople] = useState([]);
@@ -23,13 +28,29 @@ const PopularPeopleList = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <ul>
+    <div style={{ margin: '0 auto', maxWidth: '80%' }}>
       {people.map((person) => (
-        <li key={person.id}>
-          <Link to={`/person/people/${person.id}`}>{person.name}</Link>
-        </li>
+        <Accordion key={person.id}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel${person.id}-content`}
+            id={`panel${person.id}-header`}
+          >
+            <Link to={`/person/people/${person.id}`}>
+              <Typography>{person.name}</Typography>
+            </Link>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+                <h1>{person.name}</h1>
+            <img src={`https://image.tmdb.org/t/p/w500${person.profile_path}`} alt={person.name} />
+            <p><strong>Known For:</strong> {person.known_for_department}</p>
+            <p>Click Name for my details</p>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       ))}
-    </ul>
+    </div>
   );
 };
 
